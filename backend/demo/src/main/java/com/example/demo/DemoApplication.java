6127d92e459f46b6fd0a6d46e2853f781919fea7
subathraRG <727822tuit704@skct.edu.in>
2024-03-27 12:38:08 +0530

    Day6

--- backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("on default port 8080");
+		System.out.println("on default port");
 	}
 }
